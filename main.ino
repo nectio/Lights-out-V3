76a5f4d2408f832cce6530a9035fa5c76eb8841b
nectio <73787918+nectio@users.noreply.github.com>
2024-04-24 23:25:57 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,45 +1,49 @@
-// Pin definitions
-const int rel1 = 3; // the number of the LED pin
+char ingang;
+
+const int rel1 = 3;
 const int rel2 = 4;
 const int rel3 = 5;
-char ingang;
+
 const int knop1 = 11;
 const int knop2 = 10;
 const int knop3 = 9;
 const int knop4 = 8;
-// Variables will change:
-bool ledState1 = false; // variable to store the LED state
-bool ledState2 = false;
-bool ledState3 = false;
-bool ledState4 = false;
-// variable to store the LED state
-int buttonState; // variable to read the pushbutton status
-int lastButtonState1 = LOW;
-int lastButtonState2 = LOW;
-int lastButtonState3 = LOW;
-int lastButtonState4 = LOW;
+const int knop5 = 12;
+
+bool relStd1 = false; // relStd staat voor relai stand
+bool relStd2 = false;
+bool relStd3 = false;
+
+int knopStd; // knopStd staat voor knop stand
+int hrkKnopStd1 = LOW; // hrkKnopStd1 staat voor laatst herkende knop stand
+int hrkKnopStd2 = LOW;
+int hrkKnopStd3 = LOW;
+int hrkKnopStd4 = LOW;
+int hrkKnopStd5 = LOW;
 
 unsigned long vrgMilis = 0;
 const long interval = 5000; // Interval voor inactiefCon()
 bool verzenden = true;
 
-
 void setup() {
-  pinMode(knop1, OUTPUT);
-  pinMode(rel1, OUTPUT);
+  pinMode(knop1, OUTPUT); // what the fuck? ingangen moesten toch niet als OUTPUT gedeclareerd zijn???
   pinMode(knop2, OUTPUT);
-  pinMode(rel2, OUTPUT);
   pinMode(knop3, OUTPUT);
+  pinMode(knop4, OUTPUT);
+  pinMode(knop5, OUTPUT); 
+
+  pinMode(rel1, OUTPUT);
+  pinMode(rel2, OUTPUT);
   pinMode(rel3, OUTPUT);
-  pinMode(knop4, OUTPUT); // initialize the LED pin as an output
-  Serial.begin(9600); // initialize serial communication at 9600 bits per second
-  digitalWrite(rel1, ledState1);
-  digitalWrite(rel2, ledState2);
-  digitalWrite(rel3, ledState3); // set initial LED state
+  digitalWrite(rel1, relStd1); // initiële relaisstatus instellen
+  digitalWrite(rel2, relStd2);
+  digitalWrite(rel3, relStd3);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-drukknop();
+  drukknop();
   if (verzenden) {
     inactiefCon();
   }
@@ -47,7 +51,6 @@ drukknop();
   if (Serial.available() > 0) {
     char ingang = Serial.read();
     software(ingang);
-    
   }
 }
 
@@ -60,40 +63,37 @@ void inactiefCon() {
   }
 }
 
-
-
 void drukknop() {
 
-  int currentButtonState1 = digitalRead(knop1);
-  int currentButtonState2 = digitalRead(knop2);
-  int currentButtonState3 = digitalRead(knop3);
-  int currentButtonState4 = digitalRead(knop4);
-  // check if button state changed from high to low (button press)
-  if (currentButtonState1 != lastButtonState1) {
-    // only toggle the LED if the new button state is LOW
-    if (currentButtonState1 == LOW) {
-      Serial.println("S1");
-      ledState1 = !ledState1; // toggle the state of the LED
-      digitalWrite(rel1, ledState1); // set the LED accordingly
+  int hdgKnopStd1 = digitalRead(knop1);
+  int hdgKnopStd2 = digitalRead(knop2);
+  int hdgKnopStd3 = digitalRead(knop3);
+  int hdgKnopStd4 = digitalRead(knop4);
+  int hdgKnopStd5 = digitalRead(knop5);
 
-      if (ledState1) {
-        Serial.println("\033[32;49m[S1] E1 AAN");
+  if (hdgKnopStd1 != hrkKnopStd1) {               // check of de knop stand verandert is van laag naar hoog (induwen van knop1)
+    if (hdgKnopStd1 == HIGH) {                    // schakel de relais in of uit op de moment de knop ingeduwt word (HIGH)
+      Serial.println("S1");                       // geef aan in het opdrachtregel dat er een drukknop geduwt is
+      relStd1 = !relStd1;                         // schakel de stand van de relai
+      digitalWrite(rel1, relStd1);                // stel de relai in als behoort
+      if (relStd1) {
+        Serial.println("\033[32;49m[S1] E1 AAN"); // als E1 aangezet werd, geef het door aan de opdrachtregel
         rdy();
       } else {
-        Serial.println("\033[31;49m[S1] E1 UIT");
+        Serial.println("\033[31;49m[S1] E1 UIT"); // als E1 uitgezet werd, geef het door aan de opdrachtregel
         rdy();
       }
     }
   }
-  lastButtonState1 = currentButtonState1;
 
-  if (currentButtonState2 != lastButtonState2) {
-    // only toggle the LED if the new button state is LOW
-    if (currentButtonState2 == LOW) {
- Serial.println("S2");
-      ledState2 = !ledState2; // toggle the state of the LED
-      digitalWrite(rel2, ledState2); // set the LED accordingly
-      if (ledState2) {
+  hrkKnopStd1 = hdgKnopStd1; // slaag de huidige status op als de laatste status voor de volgende keer
+
+  if (hdgKnopStd2 != hrkKnopStd2) {
+    if (hdgKnopStd2 == HIGH) {
+      Serial.println("S2");
+      relStd2 = !relStd2; 
+      digitalWrite(rel2, relStd2);
+      if (relStd2) {
         Serial.println("\033[32;49m[S2] E2 AAN");
         rdy();
       } else {
@@ -103,53 +103,59 @@ void drukknop() {
     }
   }
 
-  lastButtonState2 = currentButtonState2;
+  hrkKnopStd2 = hdgKnopStd2;
 
-  if (currentButtonState3 != lastButtonState3) {
-    // only toggle the LED if the new button state is LOW
-    if (currentButtonState3 == LOW) {
-       Serial.println("S3");
-      ledState3 = !ledState3; // toggle the state of the LED
-      digitalWrite(rel3, ledState3); // set the LED accordingly
-      if (ledState3 == HIGH) {
+  if (hdgKnopStd3 != hrkKnopStd3) {
+    if (hdgKnopStd3 == HIGH) {
+      Serial.println("S3");
+      relStd3 = !relStd3;
+      digitalWrite(rel3, relStd3);
+      if (relStd3 == HIGH) {
         Serial.println("\033[32;49m[S3] E3 AAN");
         rdy();
-      } 
-      else {
+      } else {
         Serial.println("\033[31;49m[S3] E3 UIT");
         rdy();
       }
     }
   }
-  lastButtonState3 = currentButtonState3;
-  // save the current state as the last state, for next time through the loop
+  hrkKnopStd3 = hdgKnopStd3;
 
-  if (currentButtonState4 != lastButtonState4 && ledState1 == HIGH || ledState2 == HIGH || ledState3 == HIGH ) {
-    // only toggle the LED if the new button state is LOW
-    if (currentButtonState4 == HIGH) {
-       Serial.println("S4");
-      // only toggle the LED if the new button state is LOW
-   ledState3 = LOW;
-    ledState2 = LOW;
-    ledState1 = LOW;
+  if (hdgKnopStd4 != hrkKnopStd4 && relStd1 == HIGH || relStd2 == HIGH || relStd3 == HIGH) {
+    if (hdgKnopStd4 == HIGH) {
+      Serial.println("S4");
+      relStd3 = LOW;
+      relStd2 = LOW;
+      relStd1 = LOW;
       digitalWrite(rel1, LOW);
       digitalWrite(rel2, LOW);
       digitalWrite(rel3, LOW);
       Serial.println("\033[31;49m[S4] Alle lampen UIT");
-        rdy();
-
+      rdy();
     }
-    lastButtonState4 = currentButtonState4;
+    hrkKnopStd4 = hdgKnopStd4;
   }
-
-
+  if (hdgKnopStd5 != hrkKnopStd5 && relStd1 == LOW || relStd2 == LOW || relStd3 == LOW) {
+    if (hdgKnopStd5 == HIGH) {
+      Serial.println("S5");
+      relStd3 = HIGH;
+      relStd2 = HIGH;
+      relStd1 = HIGH;
+      digitalWrite(rel1, HIGH);
+      digitalWrite(rel2, HIGH);
+      digitalWrite(rel3, HIGH);
+      Serial.println("\033[31;49m[S5] Alle lampen AAN");
+      rdy();
+    }
+    hrkKnopStd5 = hdgKnopStd5;
+  }
 }
 void software(char ingang) {
-  if (ingang == '1') { // if '1' is received
-  Serial.println(ingang);
-    ledState1 = !ledState1; // toggle the state of the LED
-    digitalWrite(rel1, ledState1);
-    if (ledState1) {
+  if (ingang == '1') {
+    Serial.println(ingang);
+    relStd1 = !relStd1;
+    digitalWrite(rel1, relStd1);
+    if (relStd1) {
       Serial.println("\033[32;49m[1] E1 AAN");
       rdy();
     } else {
@@ -158,11 +164,11 @@ void software(char ingang) {
     }
   }
 
-  if (ingang == '2') { // if '1' is received
-  Serial.println(ingang);
-    ledState2 = !ledState2; // toggle the state of the LED
-    digitalWrite(rel2, ledState2); // set the LED accordingly
-    if (ledState2) {
+  if (ingang == '2') {
+    Serial.println(ingang);
+    relStd2 = !relStd2;
+    digitalWrite(rel2, relStd2);
+    if (relStd2) {
       Serial.println("\033[32;49m[2] E2 AAN");
       rdy();
     } else {
@@ -171,33 +177,44 @@ void software(char ingang) {
     }
   }
 
-  if (ingang == '3') { // if '1' is received
-  Serial.println(ingang);
-      
-    ledState3 = !ledState3; // toggle the state of the LED
-    digitalWrite(rel3, ledState3);
-    if (ledState3) {
+  if (ingang == '3') { 
+    Serial.println(ingang);
+    relStd3 = !relStd3;
+    digitalWrite(rel3, relStd3);
+    if (relStd3) {
       Serial.println("\033[32;49m[3] E3 AAN");
       rdy();
     } else {
       Serial.println("\033[31;49m[3] E3 UIT");
-       rdy();
+      rdy();
     }
   }
 
-  if (ingang == '4' || ingang == '0') { // if '1' is received
-  Serial.println(ingang);
-    ledState3 = LOW;
-    ledState2 = LOW;
-    ledState1 = LOW;
+  if (ingang == '4' || ingang == '0') { 
+    Serial.println(ingang);
+    relStd3 = LOW;
+    relStd2 = LOW;
+    relStd1 = LOW;
     digitalWrite(rel1, LOW);
     digitalWrite(rel2, LOW);
-    digitalWrite(rel3, LOW); // set the LED accordingly
+    digitalWrite(rel3, LOW); 
     Serial.println("\033[31;49m[0 / 4] Alle lampen UIT");
     rdy();
   }
 
-    if (ingang == 'h' || ingang == 'H') {
+  if (ingang == '5') { 
+    Serial.println(ingang);
+    relStd3 = HIGH;
+    relStd2 = HIGH;
+    relStd1 = HIGH;
+    digitalWrite(rel1, HIGH);
+    digitalWrite(rel2, HIGH);
+    digitalWrite(rel3, HIGH); 
+    Serial.println("\033[31;49m[5] Alle lampen AAN");
+    rdy();
+  }
+
+  if (ingang == 'h' || ingang == 'H') {
     verzenden = false;
     Serial.println(ingang);
     Serial.println("\033[91;49m========== HELPMENU ==========");
@@ -207,6 +224,7 @@ void software(char ingang) {
     Serial.println("\033[32;49m[2] E2 AAN");
     Serial.println("\033[32;49m[3] E3 AAN");
     Serial.println("\033[31;49m[0 / 4] Alle lampen UIT");
+    Serial.println("\033[31;49m[5] Alle lampen AAN");
     Serial.println("");
     Serial.println("\033[91;49m==== OPDRACHTREGELINTERFACE BEDIENINGEN ====");
     Serial.println("\033[0m[C] Opdrachtregelinterface wissen");
